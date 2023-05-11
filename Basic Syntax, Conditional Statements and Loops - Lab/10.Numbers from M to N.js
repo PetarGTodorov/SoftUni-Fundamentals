a79c58f2d2c0d7ff5e508e2a5a556b34f79c0115
PetarGTodorov <130036212+PetarGTodorov@users.noreply.github.com>
2023-05-11 14:18:21 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/10.Numbers from M to N.js
@@ -0,0 +1,6 @@
+function numbersFromMtoN(numM,numN){
+    while (numM >= numN ){
+        console.log(numM)
+        numM--;
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/9.Numbers from N to 1.js
@@ -0,0 +1,7 @@
+function numberFromNto1(num){
+while (num >= 1){
+    console.log(num)
+    num--;
+}
+}
+numberFromNto1(6)
